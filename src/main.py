854787bfa4f4da2dc959c5997921f38c6947fddc
Nicolas Flamme <nclsf87@gmail.com>
2016-07-01 12:52:45 +0300

    1st stage draft

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
 # coding=utf-8
 
+from lexer import lexer
+
+"""
+var x = 5;
+"""
+
 source = open("example.txt")
 lines_of_code = []
 for line in source:
     lines_of_code.append(line)
 source.close()
+
 c_source = translator(parser(lexer(lines_of_code)))
+
 result = open("result.c", "w")
 for line in c_source:
     result.write(line + '\n')
