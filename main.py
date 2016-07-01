6aace936fb62ccfe2a6b4e1bce18ccedfb537bd7
Nicolas Flamme <nclsf87@gmail.com>
2016-07-01 12:24:59 +0300

    1st stage draft

---
+++ main.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+source = open("example.txt")
+lines_of_code = []
+for line in source:
+    lines_of_code.append(line)
+source.close()
+c_source = translator(parser(lexer(lines_of_code)))
+result = open("result.c", "w")
+for line in c_source:
+    result.write(line + '\n')
+result.close()
