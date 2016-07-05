7377f6b0a45b0a4574d8e193e809b81b85be899e
Katsutami7moto <nclsf87@gmail.com>
2016-07-05 15:37:36 +0400

    first version of lexer is done

--- src/lexer.py
+++ src/lexer.py
@@ -2,39 +2,80 @@
 
 from regexp import returner
 
-# сначала разбить текст на слова, числа и символы
-# с помощью комбинации str.join(list) и str.split(str)
-# ограничив все символы пробелами
-# затем перевести их в токены
-
-symbols = {'+', '-', '*', '/', '%', '=', '<', '>', '(', ')', '[', ']', '{', '}', ';', ','}
-tokens = []
+symbols = {
+    '+': 'plus',
+    '-': 'minus',
+    '*': 'mul',
+    '/': 'div',
+    '%': 'mod',
+    '=': 'equal',
+    '<': 'less',
+    '>': 'more',
+    '(': 'left-paren',
+    ')': 'right-paren',
+    '[': 'left-brack',
+    ']': 'right-brack',
+    '{': 'left-curl',
+    '}': 'right-curl',
+    ';': 'semicolon',
+    ',': 'comma'
+}
+keywords = {
+    "var",
+    "const",
+    "let",
+    "del",
+    "print",
+    "input",
+    "type",
+    "new",
+    "while",
+    "do",
+    "until",
+    "if",
+    "elif",
+    "else",
+    "return"
+}
 other = ""
+tokens = []
 r_ident = "(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)" \
           "(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|0|1|2|3|4|5|6|7|8|9)*"
 r_int = "0|(1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"
 r_float = "0|(1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*\\.(0|1|2|3|4|5|6|7|8|9)+"
 r_string = "\".+\""
-r_var = "var"
-r_const = "const"
-r_let = "let"
-r_del = "del"
-r_print = "print"
-r_input = "input"
-r_type = "type"
-r_new = "new"
-r_while = "while"
-r_do = "do"
-r_until = "until"
-r_if = "if"
-r_elif = "elif"
-r_else = "else"
-r_return = "return"
 
 
-def reg_cover(reg):
+class Token:
+    def __init__(self, t, v):
+        self.type = t
+        self.value = v
+
+
+def make_token(typ, value=None):
+    return Token(typ, value)
+
+
+def r_cover(reg):
     assert isinstance(reg, str)
     return "(" + reg + ")#"
+
+
+def check(word):
+    assert isinstance(word, str)
+    if word in keywords:
+        tokens.append(make_token(word))
+    else:
+        if returner(r_cover(r_ident), word):
+            tokens.append(make_token("ident", word))
+        elif returner(r_cover(r_int), word):
+            tokens.append(make_token("int", int(word)))
+        elif returner(r_cover(r_float), word):
+            tokens.append(make_token("float", float(word)))
+        elif returner(r_cover(r_string), word):
+            tokens.append(make_token("string", word))
+        else:
+            raise NameError, "Некорректная лексема"
 
 
 def lexer(code):
@@ -47,16 +88,17 @@ def lexer(code):
                 if len(other) != 0:
                     check(other)
                     other = ""
-                tokens.append(make_sign_token(sym))
+                tokens.append(make_token(symbols[sym]))
             else:
                 other += sym
+    return tokens
 
-# if returner("(n..b)#", "n00b"):
+# if returner(r_cover("n..b"), "n00b"):
 #     print "Good!"
 # else:
 #     print "Bad!"
 #
-# if returner("(\".+\")#", '"ghjvbjdvg"'):
+# if returner(r_cover("\".+\""), '"ghjvbjdvg"'):
 #     print "Good!"
 # else:
 #     print "Bad!"
